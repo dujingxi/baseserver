f1a78e3848b8dc595cb7fddf937977b2c787a2b2
DuJingxi <dujingxi@streamocean.com>
2022-03-31 14:33:57 +0800

    add cors func

--- router.go
+++ router.go
@@ -2,7 +2,26 @@ package main
 
 import "github.com/kataras/iris/v12"
 
+// Cors
+func Cors(ctx iris.Context) {
+	ctx.Header("Access-Control-Allow-Origin", "*")
+	if ctx.Method() == "OPTIONS" {
+		ctx.Header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,OPTIONS")
+		ctx.Header("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization,File-Type,File-Name, Session-ID, Slice-Count, Slice-Number")
+		ctx.StatusCode(204)
+		return
+	}
+	ctx.Next()
+}
+
 func RegisterRouter(app *iris.Application) *iris.Application {
+	app.Use(Cors)
+	common := app.Party("/")
+	{
+		common.Options("*", func(ctx iris.Context) {
+			ctx.Next()
+		})
+	}
 	api := app.Party("/api")
 	{
 		api.Get("/", TestHandler)
